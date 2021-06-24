63ba7723e260fde359c3cfa91e648f93dee19768
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-24 07:29:52 +0000

    trailing spaces and indentation

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 const number = 15
 let guess = 0
 
-//makes event occur when button is clicked
+// makes event occur when button is clicked
 
 document.getElementById('button').addEventListener('click', play)
 
-//creates a function called play that will state the action that will occur when the button is clicked
+// creates a function called play that will state the action that will occur when the button is clicked
 
 function play () {
   // The "while" is for making a loop. This makes it so the user has to keep guessing untill they guess the right number.
@@ -15,14 +15,14 @@ function play () {
     guess = prompt('Guess my age')
     guess = parseInt(guess)
 
-  // These are for telling the user if they need to guess higher or lower.
+    // These are for telling the user if they need to guess higher or lower.
 
     if (guess > number) {
-    alert('Guess is too high, try again.')
+      alert('Guess is too high, try again.')
     } else if (guess < number) {
-    alert('Guess is too low, try again.')
+      alert('Guess is too low, try again.')
     } else if (guess === number) {
-    alert('You got it right.Yay you win !')
+      alert('You got it right.Yay you win !')
     }
   }
 }
