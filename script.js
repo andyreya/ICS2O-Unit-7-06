cf213e89cc26a344fd82c874a41ca5161cfd0c98
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-24 07:19:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// These define our variables.
+
+const number = 15
+let guess = 0
+
+//makes event occur when button is clicked
+
+document.getElementById('button').addEventListener('click', play)
+
+//creates a function called play that will state the action that will occur when the button is clicked
+
+function play () {
+  // The "while" is for making a loop. This makes it so the user has to keep guessing untill they guess the right number.
+  while (guess !== number) {
+    guess = prompt('Guess my age')
+    guess = parseInt(guess)
+
+  // These are for telling the user if they need to guess higher or lower.
+
+    if (guess > number) {
+    alert('Guess is too high.')
+    } else if (guess < number) {
+    alert('Guess is too low.')
+    } else if (guess === number) {
+    alert('you got it right')
+    }
+  }
+}
